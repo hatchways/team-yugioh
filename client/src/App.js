d64de0dc90ff3c97955a3ae892c81ae2aa13c9e4
Taras Kozak <kozaktaras15@gmail.com>
2021-01-11 22:39:53 -0500

    Set up routes for signup and login pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import LogInPage from "./pages/Login";
+import SignUpPage from "./pages/Signup";
 
 import "./App.css";
 
@@ -11,7 +12,11 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route exact path="/">
+          <Redirect to="/signup"/>
+        </Route>
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={LogInPage} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
