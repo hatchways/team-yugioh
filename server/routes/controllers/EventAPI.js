f545c224e6b6343b0a4839d03e2e8ced80bcbde5
whoanellie <NaN@nelienelson.com>
2021-01-11 23:31:19 -0800

    rebase

--- server/routes/controllers/EventAPI.js
+++ server/routes/controllers/EventAPI.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connection = require("../../db/connection");
 const db = require("../../db/models");
 
 const router = express.Router();
 
 // CREATE meeting
 router.post("/api/event", (req, res) => {
-    // sanitizing req data
+    console.log(req.body);
+    // sanitize req data
     if (req.body.user_id && req.body.duration) {
         db.EventType.create(req.body).then((response) => res.send(response));
     }
@@ -14,7 +15,10 @@ router.post("/api/event", (req, res) => {
 
 // GET all meetings for user
 router.get("/api/event", (req, res) => {
-    db.EventType.find({ user_id: req.body.user_id }).then((data) =>
+    console.log(req.query);
+    db.EventType.find({ user_id: req.query.user_id }).then((data) =>
         res.send(data)
     );
 });
+
+module.exports = router;
