99b10dfeed32defbeb3507a07784904ef74298c4
whoanellie <NaN@nelienelson.com>
2021-01-12 10:06:57 -0800

    removing comments and password.js req

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,44 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const db = require("./models");
-
-const { password } = require("./password");
+require("dotenv").config();
 
 try {
     mongoose.connect(
-        `mongodb+srv://Lil_Kuriboh:${password}@cluster0.wghil.mongodb.net/calendly?retryWrites=true&w=majority`,
+        process.env.DB_URI,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         },
         () => {
             console.log("mongo connected");
-
-            // links to tests collection
-            // db.Test.create({ id: Math.random() * 10, name: "nellie" }).then(
-            //     () => {
-            //         db.Test.find().then((data) => console.log(data));
-            //     }
-            // );
-
-            // testing appointment model
-            // db.Appointment.create({
-            //     meeting_id: "blue2617",
-            //     name: "name string",
-            //     email: "nellie@email.com",
-            //     time: new Date(),
-            //     timezone: "PST",
-            // }).then(() => {
-            //     db.Appointment.find().then((data) => console.log(data));
-            // });
-
-            //testing eventtype
-            // db.EventType.create({
-            //     user_id: 7289,
-            //     duration: 30,
-            // }).then(() => {
-            //     db.EventType.find().then((data) => console.log(data));
-            // });
         }
     );
 } catch (error) {
