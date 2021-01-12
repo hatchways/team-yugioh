d7872d6d4306f0c2a05ac6c5a147023c7034444b
alvy <alvyjudy@gmail.com>
2021-01-12 03:15:51 -0500

    add page to set availability

--- client/src/pages/OnBoarding/SetAvailability.js
+++ client/src/pages/OnBoarding/SetAvailability.js
@@ -1,28 +1,62 @@
 import React from "react";
+import {
+  Grid,
+  Typography,
+  TextField,
+  useTheme,
+  Divider,
+  Select,
+  MenuItem,
+  Menu,
+} from "@material-ui/core";
 
-import { Grid, Typography, TextField } from "@material-ui/core";
+import useStyles from "./useStylesHook";
+import ProgressBar from "./ProgressBar";
+import AvailableHours from "./smallComponents/AvailableHoursButton";
 
 const SetAvailability = (props) => {
+  //styles
+  const classes = useStyles();
+  const theme = useTheme();
+
   //two useRef objects below
   const hours = props.hours;
   const days = props.days;
 
   return (
     <div>
-      <Grid container>
-        <Typography>Set your availability</Typography>
-        <div>Progress bar...</div>
+      <Grid
+        container
+        item
+        wrap="nowrap"
+        alignItems="center"
+        justify="space-between"
+        className={classes.topContent}
+      >
+        <Typography variant="h6">Set your availability</Typography>
+        <ProgressBar start={2} end={3} />
       </Grid>
 
-      <Grid container>
-        <Typography>Available hours</Typography>
-        <TextField></TextField>
-      </Grid>
+      <Divider />
 
-      <Grid container>
-        <Typography>Select your time zone</Typography>
-        <TextField></TextField>
-      </Grid>
+      <div className={classes.pageThreeMain}>
+        <Typography className={classes.pageThreeLabel}>
+          Available hours
+        </Typography>
+
+        <Grid container className={classes.pageThreeEntry}>
+          <AvailableHours />
+          <div>--</div>
+          <AvailableHours />
+        </Grid>
+
+        <Typography className={classes.pageThreeLabel}>
+          Available days
+        </Typography>
+        <Grid container className={classes.pageThreeEntry}>
+          <TextField></TextField>
+        </Grid>
+      </div>
     </div>
   );
 };
