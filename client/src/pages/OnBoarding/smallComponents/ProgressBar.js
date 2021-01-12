aacfbfbcb3bd19ffb1f92d2d6e1ba39407c1a9d8
alvy <alvyjudy@gmail.com>
2021-01-12 04:36:29 -0500

    clean up

--- client/src/pages/OnBoarding/smallComponents/ProgressBar.js
+++ client/src/pages/OnBoarding/smallComponents/ProgressBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MobileStepper, Button, makeStyles } from "@material-ui/core";
+import { MobileStepper, makeStyles } from "@material-ui/core";
 
 const ProgressBar = (props) => {
   const classes = useStyles();
