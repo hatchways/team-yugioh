2297917e8d6242599817965c48ee1f470f3d5c29
whoanellie <NaN@nelienelson.com>
2021-01-11 16:25:44 -0800

    appointment scheme moved to correct file

---
+++ server/db/models/Appointments.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    meeting_id: {
+        type: Number,
+    },
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please fill a valid email address",
+        ],
+    },
+    time: {
+        type: Date,
+    },
+    timezone: {
+        type: String,
+    },
+});
+
+const Test = mongoose.model("Test", schema);
+
+module.exports = Test;
