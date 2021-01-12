d37ca81692c12124c50542bc0a0a4f36f6831696
alvy <alvyjudy@gmail.com>
2021-01-12 04:35:57 -0500

    add button to select available days

--- client/src/pages/OnBoarding/smallComponents/AvailableDays.js
+++ client/src/pages/OnBoarding/smallComponents/AvailableDays.js
@@ -1,5 +1,10 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Checkbox,
+  Grid,
+  FormControlLabel,
+  makeStyles,
+} from "@material-ui/core";
 
 const week = [
   "Sunday",
@@ -12,15 +17,43 @@ const week = [
 ];
 
 const AvailableDays = (props) => {
+  const classes = useStyles();
+
   const days = props.days; //useRef object
+  const [daysLocal, setDays] = useState({});
+
+  const selectDay = (event) => {
+    const newDays = { ...daysLocal, [event.target.name]: event.target.checked };
+    setDays(newDays);
+    days.current = newDays;
+  };
 
   return (
-    <Grid direction="row" container>
+    <Grid container direction="row" wrap="nowrap" alignItems="center">
       {week.map((item, i) => {
-        return <div></div>;
+        return (
+          <FormControlLabel
+            key={i}
+            label={item}
+            labelPlacement="top"
+            control={
+              <Checkbox
+                className={classes.eachDay}
+                checked={daysLocal[item]}
+                onChange={selectDay}
+              />
+            }
+          />
+        );
       })}
     </Grid>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  eachDay: {
+    margin: "0",
+  },
+}));
 
 export default AvailableDays;
