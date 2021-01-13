7f4e424d793582821c74d0cdd5b14493bcf60aaa
alvy <alvyjudy@gmail.com>
2021-01-13 00:16:28 -0500

    remove settimeout call

--- client/src/pages/OnBoarding/smallComponents/ProgressBar.js
+++ client/src/pages/OnBoarding/smallComponents/ProgressBar.js
@@ -5,14 +5,11 @@ const ProgressBar = (props) => {
   const classes = useStyles();
 
   const start = props.start || 0;
-  const end = props.end;
   const [activeStep, setActiveStep] = useState(start);
 
   useEffect(() => {
-    setTimeout(() => {
-      setActiveStep(start + 1);
-    }, 100);
-  }, [start, end]);
+    setActiveStep(start + 1);
+  }, [start]);
 
   return (
     <MobileStepper
