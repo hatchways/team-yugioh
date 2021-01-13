912951655d0966d1a11ff2014143433e149710bf
WhoaNellie <NaN@nelienelson.com>
2021-01-12 18:30:34 -0800

    added error messaging

--- server/routes/controllers/appointmentAPI.js
+++ server/routes/controllers/appointmentAPI.js
@@ -13,15 +13,33 @@ router.post("/api/appointment", (req, res) => {
         req.body.time &&
         req.body.timezone
     ) {
-        db.Appointment.create(req.body).then((response) => res.send(response));
+        db.Appointment.create(req.body)
+            .then((response) => res.send(response))
+            .catch((error) => {
+                console.log(error);
+                res.status(500).send("Unable to create appointment", error);
+            });
+    } else if (!req.body.meeting_id) {
+        res.status(400).send("Meeting ID is required");
+    } else if (!req.body.name) {
+        res.status(400).send("Name is required");
+    } else if (!req.body.email) {
+        res.status(400).send("Email is required");
+    } else if (!req.body.time) {
+        res.status(400).send("Time is required");
+    } else if (!req.body.timezone) {
+        res.status(400).send("Timezone is required");
     }
 });
 
 // GET all appointments for user
 router.get("/api/appointment", (req, res) => {
-    db.Appointment.find({ email: req.query.email }).then((data) =>
-        res.send(data)
-    );
+    db.Appointment.find({ email: req.query.email })
+        .then((data) => res.send(data))
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Unable to get appointments", error);
+        });
 });
 
 module.exports = router;
--- server/routes/controllers/eventAPI.js
+++ server/routes/controllers/eventAPI.js
@@ -7,15 +7,27 @@ const router = express.Router();
 router.post("/api/event", (req, res) => {
     // sanitize req data
     if (req.body.user_id && req.body.duration) {
-        db.EventType.create(req.body).then((response) => res.send(response));
+        db.EventType.create(req.body)
+            .then((response) => res.send(response))
+            .catch((error) => {
+                console.log(error);
+                res.status(500).send("Unable to create meeting", error);
+            });
+    } else if (!req.body.user_id) {
+        res.status(400).send("User ID is required");
+    } else if (!req.body.duration) {
+        res.status(400).send("Duration is required");
     }
 });
 
 // GET all meetings for user
 router.get("/api/event", (req, res) => {
-    db.EventType.find({ user_id: req.query.user_id }).then((data) =>
-        res.send(data)
-    );
+    db.EventType.find({ user_id: req.query.user_id })
+        .then((data) => res.send(data))
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Unable to get meetings", error);
+        });
 });
 
 module.exports = router;
