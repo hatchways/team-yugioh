c42884fc2753f8b044d6c0d09e2b411ec2eef15f
alvy <alvyjudy@gmail.com>
2021-01-13 07:49:15 -0500

    make subtitle1 bold

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -7,8 +7,24 @@ export const theme = createMuiTheme({
     h1: {
       // could customize the h1 variant as well
     },
+    h4: {
+      fontWeight: 500,
+    },
+    h5: {
+      fontWeight: 600,
+    },
+    button: {
+      textTransform: "none",
+    },
+    subtitle1: {
+      fontWeight: 600, //for bold text
+    },
   },
   palette: {
-    primary: { main: "#f68002" },
+    primary: {
+      main: "#f68002",
+      button:
+        "linear-gradient(270deg, rgba(247,132,0,1) 2%, rgba(247,105,0,1) 53%)",
+    },
   },
 });
