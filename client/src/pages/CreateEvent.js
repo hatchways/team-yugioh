3a8acba321d2a1bc4ae6bd6b387049fa1605c2e0
WhoaNellie <NaN@nelienelson.com>
2021-01-13 10:05:17 -0800

    getting and setting event types and appointments

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,37 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import mongoose from "mongoose";
 
 function CreatEvent(props) {
+    const [userEvents, setUserEvents] = useState([15, 30, 60]);
+    // replace with actual user id
+    const userId = "5ffe8c395a611a0000d0c692";
+
+    useEffect(() => {
+        axios
+            .get("/api/event?user_id=" + userId)
+            .then((res) => {
+                console.log(res);
+                const oldEventTypes = res.data.map(({ duration }) => duration);
+                setUserEvents([...userEvents, ...oldEventTypes]);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    function createNewEventType() {
+        axios
+            .post("/api/event", {
+                user_id: userId,
+                duration: Math.floor(Math.random() * 60),
+            })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
+    }
+
     return (
         <>
-            <MeetingCard type={15} />
-            <MeetingCard type={30} />
-            <MeetingCard type={60} />
+            <button onClick={createNewEventType}>Create new event type</button>
+            {userEvents.map((type) => (
+                <MeetingCard type={type} key={type} />
+            ))}
         </>
     );
 }
 
 function MeetingCard({ type }) {
-    function addEvent() {
+    function addAppointment() {
         axios
-            .post("/api/event", {
-                // user_id will need to be swapped out when we get authentication, can remove mongoose from FE at that point too
-                user_id: new mongoose.Types.ObjectId(),
-                duration: type,
+            .post("/api/appointment", {
+                // dummy data
+                meeting_id: new mongoose.Types.ObjectId(),
+                name: "test user",
+                email: "test@test.com",
+                time: new Date(),
+                timezone: "PST",
             })
             .then((res) => {
                 console.log(res);
-            });
+            })
+            .catch((err) => console.log(err));
     }
 
     return (
-        <button onClick={addEvent}>
+        <button onClick={addAppointment}>
             <h2>{type} minute meeting</h2>
         </button>
     );
 }
-
-// create new event type
 
 export default CreatEvent;
