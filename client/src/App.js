95431a5757c8911167b2f74b336c7bd8809529be
WhoaNellie <NaN@nelienelson.com>
2021-01-12 22:24:34 -0800

    test buttons to link to db.EventTypes

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,27 @@ import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { theme } from "./themes/theme";
 
 import LandingPage from "./pages/Landing";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import LogInPage from "./pages/Login";
 import SignUpPage from "./pages/Signup";
+import CreateEvent from "./pages/CreateEvent";
 
 import "./App.css";
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route exact path="/">
-          <Redirect to="/signup"/>
-        </Route>
-        <Route path="/signup" component={SignUpPage} />
-        <Route path="/login" component={LogInPage} />
-        <Route path="/landing" component={LandingPage} />        
-        <Route path="/home" component={Home} />
-
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Route exact path="/">
+                    <Redirect to="/signup" />
+                </Route>
+                <Route path="/signup" component={SignUpPage} />
+                <Route path="/login" component={LogInPage} />
+                <Route path="/landing" component={LandingPage} />
+                <Route path="/home" component={Home} />
+            </BrowserRouter>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
