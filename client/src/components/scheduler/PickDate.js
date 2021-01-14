7e1dca43c3803e444c48588d8af1569143e56c09
alvy <alvyjudy@gmail.com>
2021-01-14 09:01:55 -0500

    add material ui picker and peer dependencies

--- client/src/components/scheduler/PickDate.js
+++ client/src/components/scheduler/PickDate.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 const PickDate = () => {
   const classes = useStyles();
+
+  const [date, setDate] = useState(new Date());
+
   return (
     <Grid item xs={5} className={classes.root}>
-      pick date
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DatePicker variant="static" value={date} onChange={setDate} />
+      </MuiPickersUtilsProvider>
     </Grid>
   );
 };
 
-const useStyles = () => ({
+const useStyles = makeStyles(() => ({
   root: { height: "100%" },
-});
+}));
 
 export default PickDate;
