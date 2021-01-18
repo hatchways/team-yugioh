3ad7280ab8e05b80626ce81e11197d60fdbe48e0
Taras Kozak <kozaktaras15@gmail.com>
2021-01-18 10:55:16 -0500

    fixed 'cannot read propery of undfined' error in auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,11 +3,18 @@ const nJwt = require("njwt");
 //ensures that user has been authenticated by google
 const auth = async (req, res, next) => {
   const jwtToken=req.cookies.app_auth_token;
+  
+  if(!jwtToken){
+    res.status(401).send('')
+    return;
+  }
+    
 
   nJwt.verify(jwtToken,process.env.JWT_SECRET,function(err,verifiedJwt){
     if(err){
-      console.log(err); // Token has expired, has been tampered with, etc
+      //console.log(err); // Token has expired, has been tampered with, etc
       res.status(401).send('')
+      return;
     }else{
       next()
     }
