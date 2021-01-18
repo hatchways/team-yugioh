012ec8c7db29587dab3fff49aaa84afeb335ba92
alvy <alvyjudy@gmail.com>
2021-01-18 14:33:48 -0500

    mount checkout route

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const authenticationRout=require("./routes/controllers/authentication");
+const authenticationRout = require("./routes/controllers/authentication");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
@@ -19,36 +19,34 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(json());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
-
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
 
-
-
 app.use(require("./routes/controllers/AppointmentAPI"));
 app.use(require("./routes/controllers/eventAPI"));
+app.use(require("./routes/checkout"));
 
 app.use(authenticationRout);
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.json({ error: err });
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 module.exports = app;
