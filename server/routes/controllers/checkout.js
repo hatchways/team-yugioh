76a4000cf598a51c103f5dce33138461eac13259
alvy <alvyjudy@gmail.com>
2021-01-18 14:28:41 -0500

    checkout intent route handler

--- server/routes/controllers/checkout.js
+++ server/routes/controllers/checkout.js
@@ -2,7 +2,12 @@ const router = require("express").Router();
 
 const stripe = require("stripe").Stripe(process.env.STRIPE_SECRET);
 
-router.post("/api/checkout", (req, res) => {
+router.get("/api/checkout", async (req, res) => {
   const amount = 1024;
   const currency = "cad";
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: "cad",
+  });
+  res.status(200).json({ clientSecret: paymentIntent.client_secret, amount });
 });
