a7528b1ee43de336c57888d62e839f904ce31b48
WhoaNellie <NaN@nellienelson.com>
2021-01-18 21:25:00 -0800

    Appointment to appointment

--- server/app.js
+++ server/app.js
@@ -17,10 +17,32 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
+<<<<<<< HEAD
 //Mount route handlers
 app.use(require("./routes/AppointmentAPI"));
 app.use(require("./routes/eventAPI"));
 app.use(require("./routes/authentication"));
+=======
+app.use("/", indexRouter);
+app.use("/ping", pingRouter);
+
+app.use(require("./routes/controllers/appointmentAPI"));
+app.use(require("./routes/controllers/eventAPI"));
+app.use(require("./routes/userAPI"));
+
+app.use(authenticationRout);
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
+});
+>>>>>>> ae3f4f9... Appointment to appointment
 
 //Mount utilities
 app.use(require("./utils/errorHandler"));
