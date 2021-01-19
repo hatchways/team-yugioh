0088f0b3cb9ef8a94fd61d7d6e4a9fcc2c30696c
Taras Kozak <kozaktaras15@gmail.com>
2021-01-18 22:59:05 -0500

    set up an empty route function for get availability from google calendar

---
+++ server/routes/controllers/googleCalendarAPI.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const db = require("../../db/models");
+const { google } = require("googleapis");
+const User = require("../../db/models/User");
+const AuthStore = require("../../db/models/AuthenticationStore");
+const auth = require("../../middleware/auth");
+
+const router = new express.Router();
+
+
+
+// get user's availability from google calendar
+router.get("/api/calendar/availability", async (req, res) => {
+ 
+
+});
+
+
+
+module.exports = router;
