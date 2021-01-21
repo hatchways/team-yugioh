ba6fb3b6800ff9fe1718f464b9b97916e0975e9d
Taras Kozak <kozaktaras15@gmail.com>
2021-01-21 10:29:21 -0500

    code cleanup

--- server/routes/googleCalendarAPI.js
+++ server/routes/googleCalendarAPI.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const db = require("../db/models");
-const { google } = require("googleapis");
-const User = require("../db/models/User");
-const AuthStore = require("../db/models/AuthenticationStore");
 const auth = require("../middleware/auth");
 const getAvailability= require("../utils/googleCalendarUtils");
 
@@ -13,12 +10,17 @@ const router = new express.Router();
 // get user's availability from google calendar
 router.get("/api/calendar/availability",auth, async (req, res) => {
     const day=req.param('day');
+    try{
     const usr=await db.User.findById(req.userId);
     const tokenStore=await db.AuthStore.findOne({email:usr.email});
     const authToken=tokenStore.googleAuthToken;
-
     const availability= await getAvailability(authToken, day);
     res.status(200).send({availability});
+    }
+    catch(err){
+        res.status(500).send("error getting calendar availability")
+    }
+   
 
 });
 
