5c917329b4a45516103471533a8274e8e0c472ed
Taras Kozak <kozaktaras15@gmail.com>
2021-01-21 14:32:48 -0500

    cleanup

--- client/src/components/Routes/PrivateRoute.js
+++ client/src/components/Routes/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { testAuth } from "../../utils/googleAuth";
-import { Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ children, ...rest }) => {
   const [authenticated, setAuthenticated] = useState(false);
