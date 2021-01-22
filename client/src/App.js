c8f744d9e9c276b888931b5bb171e5c9022c79b4
alvy <alvyjudy@gmail.com>
2021-01-22 02:28:32 -0500

    set up global context to easily access and set authentication state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
@@ -13,10 +13,21 @@ import Authentication from "./pages/Authentication";
 import UpgradePage from "./pages/UpgradePage";
 import PrivateRoute from "./components/Routes/PrivateRoute";
 import PublicRoute from "./components/Routes/PublicRoute";
+import { useSetAuthenticated } from "./providers/AuthProvider";
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const setAuthenticated = useSetAuthenticated();
+  useEffect(() => {
+    axios
+      .get("/api/authentication/test", { withCredentials: true })
+      .then(() => {
+        setAuthenticated(true);
+      })
+      .catch();
+  }, []);
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import AuthProvider from "../src/providers/AuthProvider";
+import { AuthProvider } from "../src/providers/AuthProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
