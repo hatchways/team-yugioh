aeebe13afd8863ddd008986854049d118b6c7436
alvy <alvyjudy@gmail.com>
2021-01-22 02:02:53 -0500

    remove auth provider

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import AuthProvider from "../src/providers/AuthProvider";
 
-ReactDOM.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
