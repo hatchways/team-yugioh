fb8cb8c7c24963a1ae32c88cfacc3b6081102be6
alvy <alvyjudy@gmail.com>
2021-01-22 05:53:31 -0500

    set up redirection in upgrade card

--- client/src/components/upgradeCard/UpgradeCard.js
+++ client/src/components/upgradeCard/UpgradeCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
@@ -6,13 +6,14 @@ import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import PlanListItem from "./UpgradeListItem";
+import { Redirect } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-  paper:{
-    maxWidth:"300px",
-    height:"280px",
-    paddingTop:"5%",
-    paddingBottom:"5%",
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    maxWidth: "300px",
+    height: "280px",
+    paddingTop: "5%",
+    paddingBottom: "5%",
   },
   root: {
     display: "flex",
@@ -21,79 +22,110 @@ const useStyles = makeStyles(theme => ({
     width: 200,
     maxHeight: theme.spacing(26),
     textAlign: "center",
-    margin: "auto"
+    margin: "auto",
   },
   color1: {
-    color: "#7900ff"
+    color: "#7900ff",
   },
   color2: {
-    color: "#89b800"
+    color: "#89b800",
   },
   buttonRoot: {
-    marginTop:"10%",
-    margin:"auto",
-    width:"45%",
+    marginTop: "10%",
+    margin: "auto",
+    width: "45%",
     backgroundColor: "#FFF",
     "&:hover": {
       //you want this to be the same as the backgroundColor above
       backgroundColor: theme.palette.primary.main,
-      color: "white"
-    }
+      color: "white",
+    },
   },
   item: {
     padding: 0,
   },
   ListItemIcon: {
     minWidth: 0,
-    marginRight:10
+    marginRight: 10,
   },
-  icon:{
-    fontSize:16,
-    color:theme.palette.primary.main
+  icon: {
+    fontSize: 16,
+    color: theme.palette.primary.main,
   },
-  listContainer:{
-    paddingTop:"5%"
+  listContainer: {
+    paddingTop: "5%",
   },
-  itemText:{
-    color:"#898989",
-    fontSize:14,
-    fontWeight:500
+  itemText: {
+    color: "#898989",
+    fontSize: 14,
+    fontWeight: 500,
   },
-  border:{
-    borderStyle:"solid",
-    borderWidth:"2px",
-    color:theme.palette.primary.main
-  }
+  border: {
+    borderStyle: "solid",
+    borderWidth: "2px",
+    color: theme.palette.primary.main,
+  },
 }));
 
-const UpgradeCard = ({ title, titleColor, featuresList, price, selected }) => {
+const UpgradeCard = ({
+  title,
+  titleColor,
+  featuresList,
+  price,
+  selected,
+  subscribed,
+  setSubscribed,
+}) => {
   const classes = useStyles();
-  const elevation=selected?20:8;
+  const elevation = selected ? 20 : 8;
+  const [redirect, setRedirect] = useState();
 
-  return (
-    <Paper elevation={elevation} className={selected?`${classes.paper} ${classes.border}`:classes.paper}>
-      <div className={classes.root}>
-        <Typography variant="h5" className={classes[titleColor]}>
-          {title}
-        </Typography>
-        <Typography variant="subtitle1">{price}</Typography>
-        <Button
-          variant="outlined"
-          color="secondary"
-          classes={{ root: classes.buttonRoot }}
-          disabled={selected}
-        >
-          {selected?"Active":"Upgrade"}
-        </Button>
-      </div>
-      <Divider />
-      <div className={`${classes.root} ${classes.listContainer}`}>
-        <List>
-          {featuresList.map((item, idx)=>(<PlanListItem itemText={item} key={idx}/>))}
-        </List>
-      </div>
-    </Paper>
-  );
+  const upgradeOrCancelSubscription = () => {
+    if (subscribed) {
+      axios.get("/api/subscription/cancel").then(() => {
+        setSubscribed(false);
+      });
+    } else {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Redirect to="/checkout" />;
+  } else {
+    return (
+      <Paper
+        elevation={elevation}
+        className={
+          selected ? `${classes.paper} ${classes.border}` : classes.paper
+        }
+      >
+        <div className={classes.root}>
+          <Typography variant="h5" className={classes[titleColor]}>
+            {title}
+          </Typography>
+          <Typography variant="subtitle1">{price}</Typography>
+          <Button
+            variant="outlined"
+            color="secondary"
+            classes={{ root: classes.buttonRoot }}
+            disabled={selected}
+            onClick={upgradeOrCancelSubscription}
+          >
+            {selected ? "Active" : "Upgrade"}
+          </Button>
+        </div>
+        <Divider />
+        <div className={`${classes.root} ${classes.listContainer}`}>
+          <List>
+            {featuresList.map((item, idx) => (
+              <PlanListItem itemText={item} key={idx} />
+            ))}
+          </List>
+        </div>
+      </Paper>
+    );
+  }
 };
 
 export default UpgradeCard;
