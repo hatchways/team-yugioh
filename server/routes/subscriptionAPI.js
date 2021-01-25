d56f3823d400f582d0c3af8f3218d847929c9f02
alvy <alvyjudy@gmail.com>
2021-01-25 09:38:47 -0500

    use subscription model

--- server/routes/subscriptionAPI.js
+++ server/routes/subscriptionAPI.js
@@ -1,78 +1,65 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const router = require("express").Router();
-const stripe = require("stripe").Stripe(process.env.STRIPE_SECRET);
-
-const dbSubscription = require("../db/models/Subscription");
+const dbUser = require("../db/models/User");
 const auth = require("../middleware/auth");
 
 router.get("/api/subscription/check", auth, async (req, res) => {
-  // Check if the user is already subscribed. If so, set askForPayment in the response
-  // to false; otherwise set it to true
-  const user = await dbSubscription.findOne({
-    userId: req.userId,
-  });
-  if (user) {
-    res.status(200).send({ subscribed: true });
-  } else {
-    res.status(200).json({ subscribed: false });
-  }
+  const userId = req.userId;
+
+  const result = await dbUser.findOne({ _id: userId });
+
+  const subscribed = result.subscribed;
+
+  res.status(200).send({ subscribed });
 });
 
-router.get("/api/subscription/pay", async (req, res) => {
-  // Starts a payment intent and send back the transaction ID (aptly named clientSecret)
-  // It is the front end's responsibility to make sure precheckout has been run
-  // In the event that payment failed, the clientSecret would be discarded.
-  const amount = 120;
-  const currency = "cad";
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency,
-  });
-  res.status(200).json({ clientSecret: paymentIntent.client_secret, amount });
+router.post("/api/subscription/create-customer", auth, async (req, res) => {
+  const userId = req.userId;
+  const email = await dbUser.findOne({ _id: userId }).email;
+
+  const customer = await stripe.customers.create({ email: email });
+  await dbUser.updateOne({ _id: userId }, { stripeId: customer.id }); //no need to wait, but just in case
+  res.status(200).send({ customer });
 });
 
-router.post("/api/subscription/set", auth, async (req, res) => {
-  try {
-    //insert a new document into Subscription model with the user_id. There is a chance
-    //for duplication error if pre-checkout wasn't run
-    const clientSecret = req.body.clientSecret || undefined;
-    if (!clientSecret) {
-      res.status(400).send("Client secret not included in request body");
+router.post("/api/subscription/create-subscription", auth, async (req, res) => {
+  // I don't really understand this API tbh
+  const paymentMethod = await stripe.paymentMethods.attach(
+    req.body.paymentMethodId,
+    { customer: req.body.customerId }
+  );
+
+  const updateCustomerDefaultPaymentMethod = await stripe.customers.update(
+    req.body.customerId,
+    {
+      invoice_settings: {
+        default_payment_method: paymentMethod.id,
+      },
     }
-    await dbSubscription.create({
-      userId: req.userId,
-      clientSecret,
-    });
-    res.status(200).send("OK");
-  } catch (error) {
-    res.status(500).send("Error occurred in /api/post-checkout\n", error);
-  }
+  );
+
+  const subscription = await stripe.subscriptions.create({
+    customer: req.body.customerId,
+    items: [{ price: req.body.priceId }],
+    expand: ["latest_invoice.payment_intent"],
+  });
+
+  await dbUser.updateOne(
+    { _id: req.userId },
+    { subscribed: true, subscriptionId: subscription.id }
+  );
+  res.send(subscription);
 });
 
 router.get("/api/subscription/cancel", auth, async (req, res) => {
-  try {
-    await dbSubscription.deleteOne({ userId: req.userId });
-    res.status(200).send("Subscription has been canceled");
-  } catch (error) {
-    res.status(500).send("Error occurred cancelling subscription");
-  }
-});
-
-router.get(
-  "/api/subscription/development/reset-everything",
-  async (req, res) => {
-    // send a get request here to reset the model that store user subscription info
-    // will be deleted down the road but should keep it for development for now
-    const result = await dbSubscription.deleteMany();
-    res.send("OK");
-  }
-);
-
-router.get("/api/subscription/development/view", async (req, res) => {
-  // send a get request here to see the model stores user subscription info
-  // will be deleted down the road but should keep it for development for now
-  const result = await dbSubscription.find();
-  console.log(result);
-  res.send("OK");
+  const subscriptionId = (await dbUser.findOne({ _id: req.userId }))
+    .subscriptionId;
+  const subscription = await stripe.subscriptions.del(subscriptionId);
+  await dbUser.updateOne(
+    { _id: req.userId },
+    { subscribed: false, subscriptionId: "" }
+  );
+  res.status(200).send("Subscription cancelled");
 });
 
 module.exports = router;
