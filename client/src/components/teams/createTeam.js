c4012b2d080588ad59cf57e4067709b4017cb7fc
alvy <alvyjudy@gmail.com>
2021-01-26 14:31:16 -0500

    Dialog structure set up

--- client/src/components/teams/createTeam.js
+++ client/src/components/teams/createTeam.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Dialog } from "@material-ui/core";
+import {
+  Dialog,
+  makeStyles,
+  Button,
+  Typography,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+} from "@material-ui/core";
 
 const CreateTeam = ({ open, closeDialog }) => {
+  const classes = useStyles();
+
+  const createTeam = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Dialog open={open} onClose={closeDialog}>
-      Create your team here
+      <DialogTitle variant="h5">Create your team here</DialogTitle>
+      <DialogContent></DialogContent>
+      <DialogActions>
+        <Button onClick={closeDialog}>Cancel</Button>
+        <Button onClick={createTeam}>Create your team</Button>
+      </DialogActions>
     </Dialog>
   );
 };
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default CreateTeam;
