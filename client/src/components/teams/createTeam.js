75fae3a94db5fc644e75f520c0ace725344d2a3e
alvy <alvyjudy@gmail.com>
2021-01-26 14:09:25 -0500

    create a team dialog

---
+++ client/src/components/teams/createTeam.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Dialog } from "@material-ui/core";
+
+const createTeam = () => {
+  return <Dialog>Create your team here</Dialog>;
+};
+
+export default createTeam;
