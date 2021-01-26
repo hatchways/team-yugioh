7c0480d0ca9fe94d39ba174d5827e619c72e5f38
Taras Kozak <kozaktaras15@gmail.com>
2021-01-26 12:50:39 -0500

    adding comments to debouncing function

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,5 +1,8 @@
 //author medium.com/@kartikag01
 //func= function to debounce, wait=delay time
+// This function takes a function as the first argument and time in milliseconds as second argument and returns a function 
+// that runs the original function (argument 1) a maximum of once in a period of time supplied as (argument 2). 
+
 export function debounce(func, wait) {
     let timeout;
     return function(...args) {
