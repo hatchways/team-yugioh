d819370e02153c33edc51eec85133ed821621ba6
alvy <alvyjudy@gmail.com>
2021-01-26 21:11:43 -0500

    edit style

--- client/src/components/teams/TeamPage.js
+++ client/src/components/teams/TeamPage.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 
 import CreateTeam from "./CreateTeam";
 
 const TeamPage = () => {
+  const classes = useStyles();
   const [openDialog, setOpenDialog] = useState(false);
   return (
     <div>
       <Button
+        className={classes.createTeamButton}
+        variant="outlined"
+        color="secondary"
         onClick={() => {
           setOpenDialog(true);
         }}
@@ -24,4 +28,11 @@ const TeamPage = () => {
   );
 };
 
+const useStyles = makeStyles((theme) => ({
+  createTeamButton: {
+    padding: ".5rem 2rem",
+    textTransform: "none",
+    marginBotton: theme.spacing(4),
+  },
+}));
 export default TeamPage;
