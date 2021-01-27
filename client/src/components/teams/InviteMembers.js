52694c41ca7dbed915eec6c1871a93be573adba3
alvy <alvyjudy@gmail.com>
2021-01-27 13:30:20 -0500

    add an else block for when email isn't valid

--- client/src/components/teams/InviteMembers.js
+++ client/src/components/teams/InviteMembers.js
@@ -18,6 +18,8 @@ const InviteMembers = ({ setTeamMembers, teamMembers }) => {
     const typedEmail = event.target.value;
     if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(typedEmail)) {
       setEmailValid(true);
+    } else {
+      setEmailValid(false);
     }
     setEmail(typedEmail);
   };
