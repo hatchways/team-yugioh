c9b6c70ecf5ca30c6632d3fa21813090c43c5a7a
alvy <alvyjudy@gmail.com>
2021-01-27 15:48:13 -0500

    set up reschedule page (WIP)

---
+++ client/src/components/scheduler/SelectAnOpeningSlot.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+
+import PickDate from "./PickDate";
+import PickTime from "./PickTime";
+
+const SelectAnOpeningSlot = () => {
+  const classes = useStyles();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  //this will be fetched from the server
+  const availTimes = { start: "09:00", end: "17:00" };
+  const availDates = [1, 2, 3, 4, 5];
+  //this will be set when picking event type --> pulled from context?
+  const interval = "60";
+  return (
+    <div className={classes.gridContainer}>
+      <Grid container direction="column">
+        <Grid item>
+          <Typography variant="h5" className={classes.title}>
+            Select a Date {"&"} Time
+          </Typography>
+        </Grid>
+        <Grid item container spacing={2}>
+          <Grid item xs={7}>
+            <PickDate
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+              availability={availDates}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    padding: "2em",
+  },
+}));
+
+export default SelectAnOpeningSlot;
--- client/src/pages/RescheduleEvent.js
+++ client/src/pages/RescheduleEvent.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams, Route } from "react-router-dom";
+import { Grid, makeStyles, Paper, Divider } from "@material-ui/core";
+
+import Overview from "../components/scheduler/Overview";
+import SelectAnOpeningSlot from "../components/scheduler/SelectAnOpeningSlot";
+import ConfirmAppointment from "../components/scheduler/ConfirmAppointment";
 
 const RescheduleEvent = () => {
-  return <div>cancel event</div>;
+  const classes = useStyles();
+  const { eventId } = useParams();
+  console.log("event id", eventId);
+  const [eventDetails, setEventDetails] = useState({
+    name: "",
+    details: "",
+    duration: "",
+    link: "",
+    appointmentTime: "",
+  });
+
+  return (
+    <Paper className={classes.root} elevation={5}>
+      <Grid container direction="row" wrap="nowrap" className={classes.grid}>
+        <Grid item xs={4}>
+          <Overview {...eventDetails} />
+        </Grid>
+
+        <Divider orientation="vertical" flexItem={true} />
+
+        <Grid item xs={8}>
+          <Route exact path="/reschedule/:eventId">
+            <SelectAnOpeningSlot />
+          </Route>
+          <Route path="/reschedule/:eventId/confirm">
+            <ConfirmAppointment />
+          </Route>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: theme.spacing(60),
+    width: theme.spacing(100),
+    margin: `${theme.spacing(10)}px auto`,
+  },
+  grid: {
+    height: "100%",
+  },
+}));
 
 export default RescheduleEvent;
