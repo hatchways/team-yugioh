83cb0d4132b1ddea923c6343c8d819e2fcdcba10
alvy <alvyjudy@gmail.com>
2021-01-28 00:54:38 -0500

    add imports

--- client/src/pages/RescheduleOrCancelEvent.js
+++ client/src/pages/RescheduleOrCancelEvent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Redirect } from "react-router-dom";
 import { Grid, makeStyles, Paper, Divider } from "@material-ui/core";
 import axios from "axios";
 
