888cc194c5544d7b0b656cda2b9b2b4be4dbcf61
alvy <alvyjudy@gmail.com>
2021-01-30 16:12:20 -0500

    divide past and upcoming appointments

--- client/src/components/ScheduledAppointments/ScheduledAppointmentsTab.js
+++ client/src/components/ScheduledAppointments/ScheduledAppointmentsTab.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { makeStyles, Grid } from "@material-ui/core";
+import { makeStyles, Grid, Divider } from "@material-ui/core";
 import axios from "axios";
+import { parseISO, startOfToday, compareAsc } from "date-fns";
 
 import { useUserData, useSetUserData } from "../../providers/Context";
 import IndividualAppointment from "./IndividualAppointment";
-import { TrainRounded } from "@material-ui/icons";
+import YouHaveNoAppointments from "./YouHaveNoAppointments";
+import { FormatListBulletedSharp } from "@material-ui/icons";
 
 const ScheduledAppointmentsTab = () => {
   const classes = useStyles();
@@ -13,31 +15,75 @@ const ScheduledAppointmentsTab = () => {
   const userData = useUserData();
   const { _id: userId } = userData;
 
-  const [appointments, setAppointments] = useState([]);
+  const [upcomingAppointments, setupcomingAppointments] = useState([]);
+  const [pastAppointments, setPastAppointments] = useState([]);
 
   useEffect(() => {
     axios.get("/api/all-appointments").then(({ data }) => {
       // data: [{_id, email, eventId, hostId, name, time, timezone, duration},...]
-      setAppointments(data);
+      const today = startOfToday();
+      console.log("today", today);
+      const upcomingAppointments = [];
+      const pastAppointments = [];
+      data.forEach((appointment) => {
+        const appointmentDate = parseISO(appointment.time);
+
+        // comparison value:
+        // 1 if appointment is upcoming
+        // -1 if appointment is in the past
+        // 0 if the appointment is today
+        const comparison = compareAsc(appointmentDate, today);
+
+        if (comparison === -1) {
+          pastAppointments.push(appointment);
+        } else {
+          upcomingAppointments.push(appointment);
+        }
+      });
+
+      setupcomingAppointments(upcomingAppointments);
+      setPastAppointments(pastAppointments);
     });
   }, [userId]);
   return (
     <Grid container direction="column">
-      {appointments.length !== 0 &&
-        appointments.map((appointment) => {
-          return (
-            <IndividualAppointment
-              key={appointment._id}
-              appointmentId={appointment._id} //this is for child access
-              upcoming={true}
-              attendeeName={appointment.name}
-              time={appointment.time}
-              duration={appointment.duration}
-              attendeeEmail={appointment.email}
-              attendeeTimezone={appointment.timezone}
-            />
-          );
-        })}
+      {upcomingAppointments.length === 0 && (
+        <YouHaveNoAppointments upcoming={true} />
+      )}
+
+      {upcomingAppointments.length !== 0 &&
+        upcomingAppointments.map((appointment) => (
+          <IndividualAppointment
+            key={appointment._id}
+            appointmentId={appointment._id} //this is for child access
+            upcoming={true}
+            attendeeName={appointment.name}
+            time={appointment.time}
+            duration={appointment.duration}
+            attendeeEmail={appointment.email}
+            attendeeTimezone={appointment.timezone}
+          />
+        ))}
+
+      <Divider />
+
+      {pastAppointments.length === 0 && (
+        <YouHaveNoAppointments upcoming={false} />
+      )}
+
+      {pastAppointments.length !== 0 &&
+        pastAppointments.map((appointment) => (
+          <IndividualAppointment
+            key={appointment._id}
+            appointmentId={appointment._id} //this is for child access
+            upcoming={false}
+            attendeeName={appointment.name}
+            time={appointment.time}
+            duration={appointment.duration}
+            attendeeEmail={appointment.email}
+            attendeeTimezone={appointment.timezone}
+          />
+        ))}
     </Grid>
   );
 };
