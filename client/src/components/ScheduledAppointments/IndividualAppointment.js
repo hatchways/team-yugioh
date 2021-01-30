afdb32deff623d55dbe7d836c40a2f1e2f1123cf
alvy <alvyjudy@gmail.com>
2021-01-30 13:50:48 -0500

    add individual appointment accordion

--- client/src/components/ScheduledAppointments/IndividualAppointment.js
+++ client/src/components/ScheduledAppointments/IndividualAppointment.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  makeStyles,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
 
-const IndividualAppointment = ({ key }) => {
-  console.log(key);
-  return <div>hi</div>;
+const IndividualAppointment = ({
+  appointmentId,
+  upcoming,
+  attendeeName,
+  time,
+  duration,
+}) => {
+  return (
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMore />}>
+        <Grid container spacing={3}>
+          <Grid item>
+            <Typography variant="body1">{time}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1">{attendeeName}</Typography>
+            <Typography variant="body1">Event type {duration}</Typography>
+          </Grid>
+        </Grid>
+      </AccordionSummary>
+      <AccordionDetails>Details...</AccordionDetails>
+    </Accordion>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({}));
--- client/src/components/ScheduledAppointments/ScheduledAppointmentsTab.js
+++ client/src/components/ScheduledAppointments/ScheduledAppointmentsTab.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Grid } from "@material-ui/core";
 import axios from "axios";
 
 import { useUserData, useSetUserData } from "../../providers/Context";
 import IndividualAppointment from "./IndividualAppointment";
+import { TrainRounded } from "@material-ui/icons";
 
 const ScheduledAppointmentsTab = () => {
   const classes = useStyles();
@@ -16,17 +17,26 @@ const ScheduledAppointmentsTab = () => {
 
   useEffect(() => {
     axios.get("/api/all-appointments").then(({ data }) => {
-      // data: [{_id, email, eventId, hostId, name, time, timezone},...]
+      // data: [{_id, email, eventId, hostId, name, time, timezone, duration},...]
       setAppointments(data);
     });
   }, [userId]);
   return (
-    <div>
+    <Grid container direction="column">
       {appointments.length !== 0 &&
         appointments.map((appointment) => {
-          return <IndividualAppointment key={appointment._id} />;
+          return (
+            <IndividualAppointment
+              key={appointment._id}
+              appointmentId={appointment._id} //this is for child access
+              upcoming={true}
+              attendeeName={appointment.name}
+              time={appointment.time}
+              duration={appointment.duration}
+            />
+          );
         })}
-    </div>
+    </Grid>
   );
 };
 
