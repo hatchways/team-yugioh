c28e9a5ff038179190d8a4737906b5de760de853
alvy <alvyjudy@gmail.com>
2021-01-30 15:36:22 -0500

    add margin to accordion

--- client/src/components/ScheduledAppointments/IndividualAppointment.js
+++ client/src/components/ScheduledAppointments/IndividualAppointment.js
@@ -23,76 +23,83 @@ const IndividualAppointment = ({
 }) => {
   const classes = useStyles();
   return (
-    <Accordion
-      TransitionProps={{
-        timeout: 0, //allows animation to look smoother
-      }}
-    >
-      <AccordionSummary expandIcon={<ExpandMore />}>
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <Typography variant="body1">{time}</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="subtitle1">{attendeeName}</Typography>
-            <Typography variant="body1">Event type {duration}</Typography>
-          </Grid>
-        </Grid>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Grid container spacing={3}>
-          <Grid item xs={8} container direction="column" spacing={2}>
-            <Grid item>
-              <Typography variant="body1">Email: {attendeeEmail}</Typography>
+    <div className={classes.accordionWrapper}>
+      <Accordion
+        TransitionProps={{
+          timeout: 0, //allows animation to look smoother
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Grid container spacing={3}>
+            <Grid item xs={3}>
+              <Typography variant="body1">{time}</Typography>
             </Grid>
-
-            <Grid item>
-              <Typography variant="body1">
-                Attendee timezone: {attendeeTimezone}
-              </Typography>
+            <Grid item xs={4}>
+              <Typography variant="subtitle1">{attendeeName}</Typography>
+              <Typography variant="body1">Event type {duration}</Typography>
             </Grid>
           </Grid>
-          {upcoming && ( //render the reschedule and cancel button if the appointment is coming up
-            <>
-              <Divider orientation="vertical" flexItem={true} />
-              <Grid
-                item
-                xs={4}
-                container
-                direction="column"
-                alignItems="flex-end"
-                spacing={3}
-              >
-                <Grid item>
-                  <Button
-                    className={classes.button}
-                    variant="outlined"
-                    color="secondary"
-                  >
-                    Reschedule
-                  </Button>
-                </Grid>
-
-                <Grid item>
-                  <Button
-                    className={classes.button}
-                    variant="outlined"
-                    color="secondary"
-                  >
-                    Cancel
-                  </Button>
-                </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={3}>
+            <Grid item xs={8} container direction="column" spacing={2}>
+              <Grid item>
+                <Typography variant="body1">Email: {attendeeEmail}</Typography>
               </Grid>
-            </>
-          )}
-        </Grid>
-      </AccordionDetails>
-    </Accordion>
+
+              <Grid item>
+                <Typography variant="body1">
+                  Attendee timezone: {attendeeTimezone}
+                </Typography>
+              </Grid>
+            </Grid>
+            {upcoming && ( //render the reschedule and cancel button if the appointment is coming up
+              <>
+                <Divider orientation="vertical" flexItem={true} />
+                <Grid
+                  item
+                  xs={4}
+                  container
+                  direction="column"
+                  alignItems="flex-end"
+                  spacing={3}
+                >
+                  <Grid item>
+                    <Button
+                      className={classes.button}
+                      variant="outlined"
+                      color="secondary"
+                    >
+                      Reschedule
+                    </Button>
+                  </Grid>
+
+                  <Grid item>
+                    <Button
+                      className={classes.button}
+                      variant="outlined"
+                      color="secondary"
+                    >
+                      Cancel
+                    </Button>
+                  </Grid>
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  button: { width: theme.spacing(15) },
+  button: {
+    width: theme.spacing(15),
+  },
+  accordionWrapper: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 export default IndividualAppointment;
