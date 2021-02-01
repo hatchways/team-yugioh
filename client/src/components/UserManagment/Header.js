063819353adb74fea17903a1aecc2d51c6f651e3
Taras Kozak <kozaktaras15@gmail.com>
2021-02-01 08:18:15 -0500

    added edit team name field

--- client/src/components/UserManagment/Header.js
+++ client/src/components/UserManagment/Header.js
@@ -1,37 +1,64 @@
-import React from "react";
-import {
-    makeStyles,
-    Typography,
-  } from "@material-ui/core";
-  import InviteNewUsers from "./InviteNewUsers";
+import React, { useState } from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+import InviteNewUsers from "./InviteNewUsers";
+import TeamNameInput from "./TeamNameInput";
 
-  const useStyles = makeStyles(theme => ({
-   headerText:{
-       fontWeight:"normal",
-       fontSize:22,
-   },
-   header:{
-       display:"flex",
-       flexDirection:"row",
-       justifyContent:"space-between"
-   },
-   newUbutton:{
-       padding:"9px 16px",
-       fontSize:16,
-       fontWeight:"normal"
-   }
-  }));
+const useStyles = makeStyles(theme => ({
+  headerText: {
+    fontWeight: "normal",
+    fontSize: 22
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  newUbutton: {
+    padding: "9px 16px",
+    fontSize: 16,
+    fontWeight: "normal"
+  }
+}));
 
-const Header=()=>{
+const Header = () => {
+  const classes = useStyles();
+  const [editTitle, setEditTitle] = useState(false);
+  const [title, setTitle] = useState("Team 1");
 
-    const classes=useStyles();
+  const toggleEditTeamName = () => {
+    setEditTitle(!editTitle);
+  };
 
-    return(
-       <div className={classes.header}>
-           <Typography variant="h1" className={classes.headerText}>Users</Typography>
-           <InviteNewUsers/>
-       </div>
-    )
-}
+  const handleChange = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    //use teams api to change name of the team
+    toggleEditTeamName(false);
+  };
+
+  return (
+    <div className={classes.header}>
+      {editTitle ? (
+        <TeamNameInput
+          teamName={title}
+          handleChange={handleChange}
+          toggleNameChange={toggleEditTeamName}
+          handleSubmit={handleSubmit}
+        />
+      ) : (
+        <Typography
+          variant="h1"
+          className={classes.headerText}
+          onClick={toggleEditTeamName}
+        >
+          {title}
+        </Typography>
+      )}
+      <InviteNewUsers />
+    </div>
+  );
+};
 
 export default Header;
---
+++ client/src/components/UserManagment/TeamNameInput.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { makeStyles, TextField } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import CheckIcon from "@material-ui/icons/Check";
+import ClearIcon from "@material-ui/icons/Clear";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex"
+  },
+  buttonCheck: {
+    color: "green",
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: "white"
+    }
+  },
+  buttonClose: {
+    color: "red",
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: "white"
+    }
+  }
+});
+
+export default function TeamNameInput({
+  handleSubmit,
+  toggleNameChange,
+  teamName,
+  handleChange
+}) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <TextField
+        id="standard-multiline-flexible"
+        multiline
+        rowsMax={4}
+        value={teamName}
+        onChange={handleChange}
+      />
+      <div className={classes.buttonContainer}>
+        <IconButton
+          color="primary"
+          aria-label="accept"
+          component="span"
+          onClick={handleSubmit}
+          classes={{ root: classes.buttonCheck }}
+        >
+          <CheckIcon />
+        </IconButton>
+        <IconButton
+          color="primary"
+          aria-label="decline"
+          component="span"
+          onClick={toggleNameChange}
+          classes={{ root: classes.buttonClose }}
+        >
+          <ClearIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
