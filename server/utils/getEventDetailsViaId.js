589893b12726978567ab1fe0d8bc734729e02388
alvy <alvyjudy@gmail.com>
2021-02-02 17:39:48 -0500

    async keyword

--- server/utils/getEventDetailsViaId.js
+++ server/utils/getEventDetailsViaId.js
@@ -2,12 +2,12 @@ const db = require("../db/models");
 
 // Returns a promise that resolves to an object containing the details of the
 // event. This function can be optimized by hooking it onto a cache object
-const getEventDetailViaId = (eventId) => {
+const getEventDetailViaId = async (eventId) => {
   const eventDetails = await db.EventType.findOne({ _id: eventId });
   return {
     ...eventDetails.toObject(),
-    eventName: eventDetails.name
-  }
+    eventName: eventDetails.name,
+  };
 };
 
 module.exports = getEventDetailViaId;
