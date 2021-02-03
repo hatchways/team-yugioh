714ca47529f038fee78d33b2fd5ec20748721c0d
alvy <alvyjudy@gmail.com>
2021-02-03 14:25:56 -0500

    simplify minutes

--- client/src/components/onboarding/AvailableHoursButton.js
+++ client/src/components/onboarding/AvailableHoursButton.js
@@ -4,11 +4,8 @@ import PropTypes from "prop-types";
 
 const timeSlots = [];
 for (let i = 0; i < 24; i++) {
-  for (let j = 0; j < 60; j = j + 15) {
-    const hour = i < 10 ? "0" + i.toString() : i.toString();
-    const min = j < 10 ? "0" + j.toString() : j.toString();
-    timeSlots.push(hour + min);
-  }
+  const hour = i < 10 ? "0" + i.toString() : i.toString();
+  timeSlots.push(hour + ":00" + min);
 }
 
 const AvailableHoursBtn = (props) => {
