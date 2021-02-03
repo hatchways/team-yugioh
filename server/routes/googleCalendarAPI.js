6a129cb6bc738b07b411f21a8f7dedb154c19560
WhoaNellie <NaN@nellienelson.com>
2021-02-03 14:04:10 -0800

    getting availability of single member

--- server/routes/googleCalendarAPI.js
+++ server/routes/googleCalendarAPI.js
@@ -5,13 +5,14 @@ const getAvailability = require("../utils/googleCalendarUtils");
 const router = new express.Router();
 
 // get user's availability from google calendar
-router.get("/api/calendar/availability", async (req, res) => {
-  const day = req.param("day");
+router.post("/api/calendar/availability", async (req, res) => {
+  const day = req.query.day;
+  console.log(day);
   if (req.body.members.length > 1) {
     // team availability
   } else {
     try {
-      const usr = await db.User.findById(req.body.member[0].userId);
+      const usr = await db.User.findById(req.body.members[0]);
       const tokenStore = await db.AuthStore.findOne({ email: usr.email });
       const authToken = tokenStore.googleAuthToken;
       const availability = await getAvailability(authToken, day);
