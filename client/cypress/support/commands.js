91146ef166c269cfe48c12e23f014d4e080436b7
alvy <alvyjudy@gmail.com>
2021-02-03 00:32:32 -0500

    add success response stub

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,10 +1,15 @@
 const baseUrl = Cypress.config().baseUrl;
 
-Cypress.Commands.add("interceptApiAuthTestWithFailureResponse", () => {
+Cypress.Commands.add("interceptAuthTestWithFailureResponse", () => {
   // Effectively stubbing the failure response in auth.js middleware
   cy.intercept(`${baseUrl}/api/authentication/test`, { statusCode: 401 });
 });
 
-Cypress.Commands.add("interceptApiUserDataWithFailureResponse", () => {
+Cypress.Commands.add("interceptAuthTestWithSuccessResponse", () => {
+  // Effectively stubbing the failure response in auth.js middleware
+  cy.intercept(`${baseUrl}/api/authentication/test`, { statusCode: 200 });
+});
+
+Cypress.Commands.add("interceptUserDataWithFailureResponse", () => {
   cy.intercept(`${baseUrl}/api/user/data`, { statusCode: 401 });
 });
