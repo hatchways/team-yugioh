268574607f0eefa35a2bd9f91d5014cc852bb135
alvy <alvyjudy@gmail.com>
2021-02-03 01:26:20 -0500

    Better message on what's done and what's expected in tests

--- client/cypress/integration/loginLogout.spec.js
+++ client/cypress/integration/loginLogout.spec.js
@@ -12,13 +12,13 @@ describe("Display login, sign up pages", () => {
     cy.visit("/login");
   });
 
-  it("Enter email and click button", () => {
+  it("Should be able to enter email and click continue button", () => {
     cy.get("#email").type(email);
     cy.contains("Continue").click();
     cy.contains(email);
   });
 
-  it("Navigate to sign up page and return to login", () => {
+  it("Should be able to navigate to sign up page and return to login", () => {
     cy.contains("Sign up").click();
     cy.url().should("equal", `${baseUrl}/signup`);
     cy.contains("Login").click();
@@ -26,24 +26,25 @@ describe("Display login, sign up pages", () => {
   });
 });
 
-describe("Unauthenticated page returns to login", () => {
+describe("Unauthenticated page ", () => {
   beforeEach(() => {
     cy.interceptAuthTestWithFailureResponse();
     cy.interceptUserDataWithFailureResponse();
   });
 
-  it("navigate to home page", () => {
+  it("Should be redirected to login page", () => {
     cy.visit("/home");
+    cy.get(loginPageTitle);
   });
 });
 
-describe("Authenticated app root page", () => {
+describe("Authenticated and visiting root page", () => {
   beforeEach(() => {
     cy.interceptAuthTestWithSuccessResponse();
     cy.interceptUserDataWithSuccessResponse();
   });
 
-  it("redirect to home page", () => {
+  it("Should be redirect to home page", () => {
     cy.visit("/");
     cy.contains("Event Types");
   });
@@ -54,11 +55,11 @@ describe("Log out the app", () => {
     cy.interceptAuthTestWithSuccessResponse();
     cy.interceptUserDataWithSuccessResponse();
     cy.visit("/");
-  });
-
-  it("click on the log out button", () => {
     cy.get("[data-cy=userMenu]").click();
     cy.contains("Logout").click();
+  });
+
+  it("Should be redirected to login page", () => {
     cy.contains(loginPageTitle);
   });
 });
