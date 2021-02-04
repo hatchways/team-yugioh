66dc7239ec770aca472c66e805e5e65af889a37f
alvy <alvyjudy@gmail.com>
2021-02-04 00:05:54 -0500

    get event name and host name from html

--- client/src/widget/index.js
+++ client/src/widget/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import EmbeddedScheduler from "./EmbeddedScheduler";
+
 const Widget = () => {
+  const element = document.getElementById("calend-app-inline-widget");
+  const url = element.getAttribute("data-url");
+  const [hostName, eventName] = url.split("/");
+  console.log("hostName, eventName", hostName, eventName);
   return <div>Hello world</div>;
 };
 
