a65ecf58e463c46436f968058009ca30e1261972
alvy <alvyjudy@gmail.com>
2021-02-04 02:54:58 -0500

    render code block with syntax highlight

--- client/src/components/UserEvents/EmbedSchedulerDialog.js
+++ client/src/components/UserEvents/EmbedSchedulerDialog.js
@@ -4,7 +4,9 @@ import {
   DialogTitle,
   DialogContent,
   Typography,
+  makeStyles,
 } from "@material-ui/core";
+import { CopyBlock, a11yLight } from "react-code-blocks";
 
 const domain = "http://localhost:3000";
 
@@ -13,29 +15,29 @@ const EmbedSchedulerDialog = ({
   setShowEmbedInstruction,
   link,
 }) => {
+  const classes = useStyles();
   const [hostName, eventName] = link.split("/") || ["", ""];
-
+  const code = `<div id="calend-app-inline-widget" data-url="${hostName}/${eventName}" style="min-width: 320px; height: 630px;"></div>\n<script type="text/javascript" src="${domain}/assets/widget.bundle.js"></script>`;
   return (
     <Dialog
       open={showEmbedInstruction}
       onClose={() => {
         setShowEmbedInstruction(false);
       }}
+      className={classes.dialog}
+      fullWidth={true}
     >
       <DialogTitle>Embed this event in your website</DialogTitle>
       <DialogContent>
         <Typography variant="body1">
           Copy and paste the following code into your html
         </Typography>
-        <Typography variant="body1">
-          {`<div id="calend-app-inline-widget" data-url="${hostName}/${eventName}" style="min-width: 320px; height: 630px;"></div>`}
-        </Typography>
-        <Typography variant="body1">
-          {`<script type="text/javascript" src="${domain}/assets/widget.bundle.js"></script>`}
-        </Typography>
+        <CopyBlock text={code} language="javascript" theme={a11yLight} />
       </DialogContent>
     </Dialog>
   );
 };
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default EmbedSchedulerDialog;
