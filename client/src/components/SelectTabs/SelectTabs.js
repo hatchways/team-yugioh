d84848de202c54a4168cf0cd6376f34e1b82c1a2
Matt <mattcharlesh@gmail.com>
2021-02-04 17:31:20 -0500

    corrected Select Tabs for mobile

--- client/src/components/SelectTabs/SelectTabs.js
+++ client/src/components/SelectTabs/SelectTabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
@@ -34,7 +34,6 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired,
 };
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -51,7 +50,7 @@ const useStyles = makeStyles((theme) => ({
 
 const StyledTab = withStyles((theme) => ({
   root: {
-    minWidth: 72,
+    minWidth: 50,
     fontWeight: theme.typography.fontWeightRegular,
     marginRight: theme.spacing(4),
     fontSize: "1rem",
@@ -74,24 +73,56 @@ export default function SelectTabs() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  // ADDING RESPONSIVE SCREEN SIZE FOR MOBILE
+  const [state, setState] = useState({
+    mobileView: false,
+  });
+  const { mobileView } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 600
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <Tabs
+        direction="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <StyledTab label="Event Types" />
+        <StyledTab label="Scheduled Appointments" />
+        <StyledTab label="Teams" />
+      </Tabs>
+    );
+  };
+
+  const displayMobile = () => {
+    return (
+      <Tabs
+        direction="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <StyledTab style={{ width: "50px" }} label="Event Types" />
+        <StyledTab style={{ width: "50px" }} label="Sched. Appts" />
+        <StyledTab style={{ width: "50px" }} label="Teams" />
+      </Tabs>
+    );
+  };
 
   return (
     <Container className={classes.root} maxWidth="xl">
       <div position="static">
-        <Tabs
-        direction="vertical"
-          value={value}
-          onChange={handleChange}
-          aria-label="simple tabs example"
-        >
-          <StyledTab label="Event Types" />
-          <StyledTab
-            classes={classes.tab}
-            label="Scheduled Appointments"
-            
-          />
-          <StyledTab label="Teams" />
-        </Tabs>
+        {mobileView ? displayMobile() : displayDesktop()}
       </div>
       <TabPanel value={value} index={0}>
         <EventTypesTab />
@@ -105,4 +136,3 @@ export default function SelectTabs() {
     </Container>
   );
 }
-
