fb369851ad14c5a5f59e35d627b28efba779c5f9
WhoaNellie <NaN@nellienelson.com>
2021-02-04 21:56:59 -0800

    added calendy to subscription confirmation page

--- client/src/components/Checkout/PaymentSuccess.js
+++ client/src/components/Checkout/PaymentSuccess.js
@@ -3,6 +3,7 @@ import { Typography, makeStyles, Grid, Button } from "@material-ui/core";
 import { CheckCircleOutline } from "@material-ui/icons";
 import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
+import Cash from "../../assets/cash.png";
 
 const PaymentSuccess = () => {
   const classes = useStyles();
@@ -15,17 +16,35 @@ const PaymentSuccess = () => {
         direction="column"
         alignItems="center"
         justify="space-between"
-        spacing={4}
+        spacing={1}
       >
         <Grid item>
-          <Typography variant="h5">Thanks for your subscription</Typography>
+          <Grid
+            container
+            direction="row"
+            align="center"
+            className={classes.thanks}
+          >
+            <Grid item>
+              <CheckCircleOutline className={classes.checkCircle} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">
+                Thank you for your subscription!
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <img
+            className={classes.calendy}
+            src={Cash}
+            alt="cute calendar holding cash"
+          />
         </Grid>
         <Grid item>
           <Typography variant="subtitle2">Keep your reference:</Typography>
           <Typography variant="subtitle2">{reference}</Typography>
-        </Grid>
-        <Grid item>
-          <CheckCircleOutline className={classes.checkCircle} />
         </Grid>
       </Grid>
       <Grid container justify="center">
@@ -45,14 +64,22 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(9),
     paddingBottom: theme.spacing(3),
   },
+  calendy: {
+    width: "40%",
+    display: "block",
+    margin: "auto",
+  },
   checkCircle: {
     color: "green",
-    height: theme.spacing(6),
+    height: theme.spacing(3),
     width: theme.spacing(6),
   },
   link: {
     textDecoration: "none",
     color: theme.palette.common.white,
+  },
+  thanks: {
+    marginLeft: "-4%",
   },
   button: {
     background: theme.palette.primary.button,
